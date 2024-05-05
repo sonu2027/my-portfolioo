82eb009ecf0b5bad7e3687281724f9e84f9d8394
Sonu Mondal <takeinfo2u@gmail.com>
2024-05-05 13:31:19 +0530

    added project section

--- src/index.js
+++ src/index.js
@@ -38,82 +38,127 @@ hamburgerMenu.addEventListener("click", () => {
     navItems.appendChild(navUl);
 
     const skillBar = document.querySelectorAll("ul");
-    console.log("skill", skillBar[1].children[1]);
-    const skill = document.getElementsByClassName("skill");
-    skillBar[1].children[1].addEventListener("click", () => {
-      navItems.removeChild(navItems.querySelector("ul"));
-      xmark.style.display = "none";
-      bar.style.display = "block";
-      skill[0].scrollIntoView({ behavior: "smooth", block: "start" });
-    });
 
-    const intro = document.getElementById("intro");
+    let currentScrollPos = window.scrollY;
+    if (currentScrollPos >= 0 && currentScrollPos < 650) {
+      skillBar[1].children[0].style.color = "blue";
+      skillBar[1].children[0].style.textDecoration = "underline";
+
+      skillBar[1].children[1].style.color = "black";
+      skillBar[1].children[1].style.textDecoration = "none";
+
+      skillBar[1].children[2].style.color = "black";
+      skillBar[1].children[2].style.textDecoration = "none";
+    } else if (currentScrollPos >= 650 && currentScrollPos < 1800) {
+      skillBar[1].children[0].style.color = "black";
+      skillBar[1].children[0].style.textDecoration = "none";
+
+      skillBar[1].children[1].style.color = "blue";
+      skillBar[1].children[1].style.textDecoration = "underline";
+
+      skillBar[1].children[2].style.color = "black";
+      skillBar[1].children[2].style.textDecoration = "none";
+    } else if (currentScrollPos >= 1800) {
+      skillBar[1].children[0].style.color = "black";
+      skillBar[1].children[0].style.textDecoration = "none";
+
+      skillBar[1].children[1].style.color = "black";
+      skillBar[1].children[1].style.textDecoration = "none";
+
+      skillBar[1].children[2].style.color = "blue";
+      skillBar[1].children[2].style.textDecoration = "underline";
+    } else {
+      skillBar[1].children[0].style.color = "black";
+      skillBar[1].children[1].style.color = "black";
+      skillBar[1].children[0].style.textDecoration = "none";
+      skillBar[1].children[1].style.textDecoration = "none";
+    }
+
     skillBar[1].children[0].addEventListener("click", () => {
       navItems.removeChild(navItems.querySelector("ul"));
       xmark.style.display = "none";
       bar.style.display = "block";
-      intro.scrollIntoView({ behavior: "smooth", block: "start" });
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth", // optional: for smooth scrolling
+      });
     });
 
-    window.addEventListener("scroll", () => {
-      let currentScrollPos = window.scrollY;
-      if (currentScrollPos >= 736) {
-        skillBar[1].children[0].style.color = "black";
-        skillBar[1].children[0].style.textDecoration = "none";
-        skillBar[1].children[1].style.color = "blue";
-        skillBar[1].children[1].style.textDecoration = "underline";
-      } else if (currentScrollPos >= 0 && currentScrollPos < 736) {
-        skillBar[1].children[1].style.color = "black";
-        skillBar[1].children[1].style.textDecoration = "none";
-        skillBar[1].children[0].style.color = "blue";
-        skillBar[1].children[0].style.textDecoration = "underline";
-      } else {
-        skillBar[1].children[0].style.color = "black";
-        skillBar[1].children[1].style.color = "black";
-        skillBar[1].children[0].style.textDecoration = "none";
-        skillBar[1].children[1].style.textDecoration = "none";
-      }
+    skillBar[1].children[1].addEventListener("click", () => {
+      navItems.removeChild(navItems.querySelector("ul"));
+      xmark.style.display = "none";
+      bar.style.display = "block";
+      window.scrollTo({
+        top: 650,
+        behavior: "smooth", // optional: for smooth scrolling
+      });
+    });
 
-      console.log(
-        "currentScrollPos",
-        currentScrollPos,
-        skillBar[0].children[1]
-      );
+    skillBar[1].children[2].addEventListener("click", () => {
+      navItems.removeChild(navItems.querySelector("ul"));
+      xmark.style.display = "none";
+      bar.style.display = "block";
+      console.log("1350 is running");
+      window.scrollTo({
+        top: 1800,
+        behavior: "smooth", // optional: for smooth scrolling
+      });
     });
   }
 });
 
 const skillBar = document.querySelectorAll("ul");
-console.log("skill", skillBar[0].children[1]);
-const skill = document.getElementsByClassName("skill");
-skillBar[0].children[1].addEventListener("click", () => {
-  skill[0].scrollIntoView({ behavior: "smooth", block: "start" });
-});
 
-skillBar[0].children[2].addEventListener("click", () => {
+skillBar[0].children[0].addEventListener("click", () => {
   window.scrollTo({
-    top: 600,
+    top: 0,
     behavior: "smooth", // optional: for smooth scrolling
   });
 });
 
-const intro = document.getElementById("intro");
-skillBar[0].children[0].addEventListener("click", () => {
-  intro.scrollIntoView({ behavior: "smooth", block: "start" });
+skillBar[0].children[1].addEventListener("click", () => {
+  window.scrollTo({
+    top: 650,
+    behavior: "smooth", // optional: for smooth scrolling
+  });
+});
+
+skillBar[0].children[2].addEventListener("click", () => {
+  window.scrollTo({
+    top: 1350,
+    behavior: "smooth", // optional: for smooth scrolling
+  });
 });
 
 window.addEventListener("scroll", () => {
   let currentScrollPos = window.scrollY;
-  if (currentScrollPos >= 380) {
-    skillBar[0].children[0].style.color = "black";
-    skillBar[0].children[0].style.textDecoration = "none";
-    skillBar[0].children[1].style.color = "blue";
-    skillBar[0].children[1].style.textDecoration = "underline";
-  } else if (currentScrollPos >= 0 && currentScrollPos < 380) {
-    skillBar[0].children[1].style.color = "black";
-    skillBar[0].children[1].style.textDecoration = "none";
+  if (currentScrollPos >= 0 && currentScrollPos < 650) {
     skillBar[0].children[0].style.color = "blue";
     skillBar[0].children[0].style.textDecoration = "underline";
+
+    skillBar[0].children[1].style.color = "black";
+    skillBar[0].children[1].style.textDecoration = "none";
+
+    skillBar[0].children[2].style.color = "black";
+    skillBar[0].children[2].style.textDecoration = "none";
+  } else if (currentScrollPos >= 650 && currentScrollPos < 1350) {
+    skillBar[0].children[0].style.color = "black";
+    skillBar[0].children[0].style.textDecoration = "none";
+
+    skillBar[0].children[1].style.color = "blue";
+    skillBar[0].children[1].style.textDecoration = "underline";
+
+    skillBar[0].children[2].style.color = "black";
+    skillBar[0].children[2].style.textDecoration = "none";
+  } else if (currentScrollPos >= 1350) {
+    skillBar[0].children[0].style.color = "black";
+    skillBar[0].children[0].style.textDecoration = "none";
+
+    skillBar[0].children[1].style.color = "black";
+    skillBar[0].children[1].style.textDecoration = "none";
+
+    skillBar[0].children[2].style.color = "blue";
+    skillBar[0].children[2].style.textDecoration = "underline";
   } else {
     skillBar[0].children[0].style.color = "black";
     skillBar[0].children[1].style.color = "black";
@@ -122,4 +167,12 @@ window.addEventListener("scroll", () => {
   }
 
   console.log("currentScrollPos", currentScrollPos, skillBar[0].children[1]);
+
+
+  console.log("(navItems.querySelector66", navItems.querySelector("ul"));
+  // if (navItems.querySelector("ul")!=null) {
+  //   navItems.removeChild(navItems.querySelector("ul"));
+  //   xmark.style.display = "none";
+  //   bar.style.display = "block";
+  // }
 });
